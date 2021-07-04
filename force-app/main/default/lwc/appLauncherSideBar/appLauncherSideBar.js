6eba2382ad0434d52810d3eedbd54427bbf829f2
ildiavolorosso <morse.joe@gmail.com>
2021-07-03 20:05:16 -0700

    Added placeholder properties

--- force-app/main/default/lwc/appLauncherSideBar/appLauncherSideBar.js
+++ force-app/main/default/lwc/appLauncherSideBar/appLauncherSideBar.js
@@ -3,4 +3,6 @@ import { LightningElement, api } from 'lwc';
 export default class AppLauncherSideBar extends LightningElement {
     @api tabText = 'My Apps';
     @api menuHeader = 'Key Tools';
+    @api ignoreAppLauncherProfilePerm = false;
+    @api showInternalApps = false;
 }
