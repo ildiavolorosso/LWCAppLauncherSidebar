5efe79724bcdd0b121a752390fbc67c86ef5a2d8
Arjun Srikakulam Sreenath <asreenath@asreenath-ltm.internal.salesforce.com>
2021-07-09 23:06:20 +0530

    changing js and html

--- force-app/main/default/lwc/appLauncherSideBar/appLauncherSideBar.js
+++ force-app/main/default/lwc/appLauncherSideBar/appLauncherSideBar.js
@@ -5,4 +5,13 @@ export default class AppLauncherSideBar extends LightningElement {
     @api menuHeader = 'Key Tools';
     @api ignoreAppLauncherProfilePerm = false;
     @api showInternalApps = false;
+    openNav(){
+        this.template.querySelector('[data-id="mySidebar"]').style.width = "250px";
+        this.template.querySelector('[data-id="main_menu"]').style.width = "250px";
+    }
+    closeNav(){
+        this.template.querySelector('[data-id="mySidebar"]').style.width = "0";
+        this.template.querySelector('[data-id="main_menu"]').style.width = "250";
+    }
 }
+
